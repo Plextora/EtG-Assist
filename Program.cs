faf8b86950587b2fde6e755da57a0b47922f8cee
Plextora <plextoraa.a@gmail.com>
2022-07-11 12:46:33 -0400

    Added memoryManager

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿using Memory;
+using System.Threading;
 
 namespace EtG_Assist
 {
     internal class Program
     {
+
+        Mem memoryManager = new Mem();
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
